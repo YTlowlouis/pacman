d26eaba146e551e8fb48d2e59beb13ef5ebedc3b
YTlowlouis <mottemotte350@gmail.com>
2026-09-03 20:17:12 +0200

    began main program, add json module and argparse, created Engine and parser_models.py

---
+++ pacman.py
@@ -0,0 +1,19 @@
+from src.engine.engine import Engine, ConfigFileError
+import argparse
+
+
+class Main:
+    def __init__(self):
+        parser = argparse.ArgumentParser(prog="pacman")
+        parser.add_argument("configfile", default="config.json")
+        args = parser.parse_args()
+        config_file = args.configfile
+
+        try:
+            engine = Engine(config_file)
+        except ConfigFileError as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main = Main()
--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,3 +1,20 @@
+import json
+
+
+class ConfigFileError(Exception):
+    pass
+
+
 class Engine:
-    def __init__(self):
-        pass
+    def __init__(self, config_file: str):
+        self.load_conf(config_file)
+
+    def load_conf(self, config_file: str) -> None:
+        try:
+            with open(config_file, "r") as file:
+                text = "".join(
+                    line for line in file if not line.startswith("#")
+                )
+            options = json.loads(text)
+        except Exception as e:
+            raise ConfigFileError(f"{e}")
