57e7f24d98e4972d052d1f7a7d43dcf327160947
Louis Boehm <loboehm@c4r2p5.42nice.fr>
2026-09-18 16:06:02 +0200

    change sprites base values, added movements for pacman and pacgums

--- src/sprites/pacman.py
+++ src/sprites/pacman.py
@@ -1,13 +1,39 @@
 from pathlib import Path
-from src.sprites.base import Sprite  #type: ignore
+from src.sprites.base import Sprite  # type: ignore
 
 
 class PacMan(Sprite):
-    def __init__(self, lives: int, pos: tuple[int, int], alive: bool,
-                 visible: bool, dir: str, can_eat: bool, next_dir: str,
-                 respawn_coord: tuple[int, int], super_power: bool, sprite: Path) -> None:
-        super().__init__(pos, 0, visible, sprite, lives, alive, dir,
-                         can_eat, False, next_dir, respawn_coord, super_power)
+    def __init__(
+        self,
+        lives: int,
+        pos: tuple[int, int],
+        alive: bool,
+        visible: bool,
+        dir: str,
+        can_eat: bool,
+        next_dir: str,
+        respawn_coord: tuple[int, int],
+        super_power: bool,
+        sprite: Path,
+        target: tuple[int, int],
+        progress: float,
+    ) -> None:
+        super().__init__(
+            pos,
+            0,
+            visible,
+            sprite,
+            lives,
+            alive,
+            dir,
+            can_eat,
+            False,
+            next_dir,
+            respawn_coord,
+            super_power,
+            target,
+            progress,
+        )
         self.points: int = 0
         self.VALID_DIRECTIONS = {"up", "down", "right", "left"}
 
